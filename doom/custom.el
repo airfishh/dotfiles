10d8b32bbc156f450ca1e74975bd6316e7fc6fdd
airfishh <169735261+airfishh@users.noreply.github.com>
2025-11-04 10:15:31 +1300

    Initial commit

---
+++ doom/custom.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fd22a3aac273624858a4184079b7134fb4e97104d1627cb2b488821be765ff17" default))
+ '(org-agenda-files '("~/Documents/org-mode-files/test.org"))
+ '(package-selected-packages
+   '(go graphviz-dot-mode org-latex-impatient org-modern org-roam org-roam-ql
+     org-roam-ui pdf-tools pytest treemacs-evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#3C3836" :foreground "#bbc2cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 126 :width normal :foundry "UKWN" :family "Maple Mono")))))
